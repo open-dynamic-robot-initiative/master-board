4156e8f87726f84061a3156b6ee60272268c4d4d
Etienne <etienne.arlaud@free.fr>
2019-07-19 14:24:30 +0200

    Copy_Paste direct_wifi implementation

--- components/direct_wifi/direct_wifi.c
+++ components/direct_wifi/direct_wifi.c
@@ -1,1 +1,47 @@
 #include "direct_wifi.h"
+
+esp_now_peer_info_t peer;
+static uint8_t s_example_broadcast_mac[ESP_NOW_ETH_ALEN] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
+
+void wifi_send_data(uint8_t *data, int len) {
+    esp_now_send(peer.peer_addr, data, len);
+}
+
+static void wifi_send_cb(const uint8_t *mac_addr, esp_now_send_status_t status)
+{
+  if(status != ESP_OK) {
+    //error_send++;
+  }
+}
+
+void wifi_recv_func(const uint8_t *mac, const uint8_t *rxData, int len) {
+
+}
+
+void wifi_init() {
+/* Init WiFi */
+    tcpip_adapter_init();
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    cfg.ampdu_tx_enable = 0;
+    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK( esp_wifi_start() );
+    ESP_ERROR_CHECK( esp_wifi_set_channel(CONFIG_CHANNEL, WIFI_SECOND_CHAN_NONE) );
+    ESP_ERROR_CHECK( esp_wifi_internal_set_fix_rate(ESP_IF_WIFI_STA, true, WIFI_PHY_RATE_24M) );
+
+
+    /* Init ESPNOW */
+    ESP_ERROR_CHECK( esp_now_init() );
+    ESP_ERROR_CHECK( esp_now_register_send_cb(wifi_send_cb) );
+    ESP_ERROR_CHECK( esp_now_register_recv_cb(wifi_recv_func) );
+    
+    memset(&peer, 0, sizeof(esp_now_peer_info_t));
+    peer.channel = CONFIG_CHANNEL;
+    peer.ifidx = ESP_IF_WIFI_STA;
+    peer.encrypt = false;
+    memcpy(peer.peer_addr, s_example_broadcast_mac, ESP_NOW_ETH_ALEN);
+    ESP_ERROR_CHECK( esp_now_add_peer(&peer) );
+  }
--- components/direct_wifi/direct_wifi.h
+++ components/direct_wifi/direct_wifi.h
@@ -1,14 +1,46 @@
 #ifndef DIRECT_WIFI_H
 #define DIRECT_WIFI_H
 
-/*
+#include <stdlib.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/semphr.h"
+#include "freertos/timers.h"
+#include "esp_event.h"
+#include "tcpip_adapter.h"
+#include "esp_wifi.h"
+#include "esp_log.h"
+#include "esp_system.h"
+#include "esp_now.h"
+#include "/home/earlaud/.arduino15/packages/esp32/hardware/esp32/1.0.1/tools/sdk/include/esp32/esp_wifi_internal.h"
+
+
+#if CONFIG_DATARATE_6
+  #define CONFIG_DATARATE WIFI_PHY_RATE_6M
+#elif CONFIG_DATARATE_9
+  #define CONFIG_DATARATE WIFI_PHY_RATE_9M
+#elif CONFIG_DATARATE_12
+  #define CONFIG_DATARATE WIFI_PHY_RATE_12M
+#elif CONFIG_DATARATE_18
+  #define CONFIG_DATARATE WIFI_PHY_RATE_18M
+#elif CONFIG_DATARATE_24
+  #define CONFIG_DATARATE WIFI_PHY_RATE_24M
+#elif CONFIG_DATARATE_36
+  #define CONFIG_DATARATE WIFI_PHY_RATE_36M
+#elif CONFIG_DATARATE_48
+  #define CONFIG_DATARATE WIFI_PHY_RATE_48M
+#elif CONFIG_DATARATE_56
+  #define CONFIG_DATARATE WIFI_PHY_RATE_56M
+#else
+  #define CONFIG_DATARATE WIFI_PHY_RATE_ERROR
+#endif
+
 void (*wifi_recv_cb)(uint8_t src_mac[6], uint8_t *data, int len);
 
 void wifi_init();
-esp_err_t wifi_send_frame(eth_frame *p_frame);
-void wifi_init_frame(eth_frame *p_frame);
+void wifi_send_data(uint8_t *data, int len);
 void wifi_attach_recv_cb(void (*cb)(uint8_t src_mac[6], uint8_t *data, int len));
 void wifi_detach_recv_cb();
-*/
+
 
 #endif
