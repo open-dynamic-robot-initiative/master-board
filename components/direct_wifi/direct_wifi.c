05a9e1db5162fa3d5f47b852d18af9c0ee1dc56b
Etienne <etienne.arlaud@free.fr>
2019-07-19 13:56:18 +0200

    Adding wifi_direct

---
+++ components/direct_wifi/direct_wifi.c
@@ -0,0 +1,1 @@
+#include "direct_wifi.h"
---
+++ components/direct_wifi/direct_wifi.h
@@ -0,0 +1,14 @@
+#ifndef DIRECT_WIFI_H
+#define DIRECT_WIFI_H
+
+/*
+void (*wifi_recv_cb)(uint8_t src_mac[6], uint8_t *data, int len);
+
+void wifi_init();
+esp_err_t wifi_send_frame(eth_frame *p_frame);
+void wifi_init_frame(eth_frame *p_frame);
+void wifi_attach_recv_cb(void (*cb)(uint8_t src_mac[6], uint8_t *data, int len));
+void wifi_detach_recv_cb();
+*/
+
+#endif
