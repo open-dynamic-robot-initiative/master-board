554b492e36f7fe22120c6aa12e800aa0d51fa54d
Etienne <etienne.arlaud@free.fr>
2019-07-24 14:57:04 +0200

    Fix CRC spi

--- components/spi_manager/spi_quad_packet.c
+++ components/spi_manager/spi_quad_packet.c
@@ -3,7 +3,7 @@
 #include "quad_crc.h"
 
 uint32_t packet_compute_CRC(spi_packet *packet) {
-  return CRC_compute((void*) packet, sizeof(packet) - sizeof(uint32_t));
+  return CRC_compute((void*) packet, sizeof(spi_packet) - sizeof(uint32_t));
 }
 
 bool packet_check_CRC(spi_packet *packet) {
