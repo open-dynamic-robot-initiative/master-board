0e052ffc969f46a6cd5cb1d4149000ffd08c864a
Etienne <etienne.arlaud@free.fr>
2019-07-25 09:46:47 +0200

    Script at 1 kHz

--- scripts/src/main.cpp
+++ scripts/src/main.cpp
@@ -8,6 +8,8 @@ Etienne Arlaud
 #include "ETHERNET_manager.h"
 #include "spi_quad_packet.h"
 
+#include <chrono>
+
 static uint8_t my_mac[6] = {0xF8, 0x1A, 0x67, 0xb7, 0xEB, 0x0B};
 static uint8_t dest_mac[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 static uint8_t ESP_mac[6] = {0xb4, 0xe6, 0x2d, 0xb5, 0x9f, 0x85}; //{0xcc,0x50,0xe3,0xB6,0xb4,0x58};
@@ -49,31 +51,36 @@ int main(int argc, char **argv) {
 	handler->set_dst_mac(dest_mac);
 	
 	int n_count = 0;
+	std::chrono::time_point<std::chrono::system_clock> last;
 
 	while(1) {
-		if(n_count < 10) {
-			slaves_packets[1].payload.control.cmd = 0x1;
-	        slaves_packets[1].payload.control.payload.commands.present_flags.flags = 0;
-	        slaves_packets[1].payload.control.payload.commands.present_flags.recv_Iq_timeout = 0;
-	        slaves_packets[1].payload.control.payload.commands.flags_values.flags = 0;
+		if(((std::chrono::duration<double>) (std::chrono::system_clock::now() - last)).count() > 0.001) {
+			if(n_count < 10) {
+				slaves_packets[1].payload.control.cmd = 0x1;
+		        slaves_packets[1].payload.control.payload.commands.present_flags.flags = 0;
+		        slaves_packets[1].payload.control.payload.commands.present_flags.recv_Iq_timeout = 0;
+		        slaves_packets[1].payload.control.payload.commands.flags_values.flags = 0;
 
-	        slaves_packets[1].payload.control.payload.commands.present_flags.flags |= SPI_CONTROL_CMD_FLAGS_ENA_SYS | SPI_CONTROL_CMD_FLAGS_ENA_MTR1 | SPI_CONTROL_CMD_FLAGS_ENA_MTR2;
-	        slaves_packets[1].payload.control.payload.commands.flags_values.flags |= SPI_CONTROL_CMD_FLAGS_ENA_SYS | SPI_CONTROL_CMD_FLAGS_ENA_MTR1 | SPI_CONTROL_CMD_FLAGS_ENA_MTR2;
+		        slaves_packets[1].payload.control.payload.commands.present_flags.flags |= SPI_CONTROL_CMD_FLAGS_ENA_SYS | SPI_CONTROL_CMD_FLAGS_ENA_MTR1 | SPI_CONTROL_CMD_FLAGS_ENA_MTR2;
+		        slaves_packets[1].payload.control.payload.commands.flags_values.flags |= SPI_CONTROL_CMD_FLAGS_ENA_SYS | SPI_CONTROL_CMD_FLAGS_ENA_MTR1 | SPI_CONTROL_CMD_FLAGS_ENA_MTR2;
+			} else {
+				slaves_packets[1].payload.control.cmd = 0x0;
+		        slaves_packets[1].payload.control.payload.commands.present_flags.flags = 0;
+		        slaves_packets[1].payload.control.payload.commands.present_flags.recv_Iq_timeout = 0;
+		        slaves_packets[1].payload.control.payload.commands.flags_values.flags = 0;
+
+				slaves_packets[1].payload.control.payload.values.Iq[0] = 25;
+				slaves_packets[1].payload.control.payload.values.Iq[1] = -25;
+			}
+			
+			last = std::chrono::system_clock::now();
+
+			handler->send((uint8_t *) slaves_packets, sizeof(spi_packet)*6),
+
+			n_count++;
 		} else {
-			slaves_packets[1].payload.control.cmd = 0x0;
-	        slaves_packets[1].payload.control.payload.commands.present_flags.flags = 0;
-	        slaves_packets[1].payload.control.payload.commands.present_flags.recv_Iq_timeout = 0;
-	        slaves_packets[1].payload.control.payload.commands.flags_values.flags = 0;
-
-			slaves_packets[1].payload.control.payload.values.Iq[0] = 25;
-			slaves_packets[1].payload.control.payload.values.Iq[1] = -25;
+			std::this_thread::yield();
 		}
-		
-		handler->send((uint8_t *) slaves_packets, sizeof(spi_packet)*6),
-		std::this_thread::sleep_for(std::chrono::milliseconds(100));
-		
-		n_count++;
-		//std::this_thread::yield();
 	}
 
 	handler->end();
