4809bbcd5fcfb01654b15312380445e521fdc8b0
Thomas Flayols <tflayols@daisen.laas.fr>
2019-07-31 13:10:01 +0200

    Close the loop with a PD controller for one motor

--- software/scripts/src/main.cpp
+++ software/scripts/src/main.cpp
@@ -9,6 +9,7 @@ Etienne Arlaud
 #include "spi_quad_packet.h"
 
 #include <chrono>
+#include <math.h>
 
 #define N_SLAVES 6
 
@@ -43,13 +44,27 @@ Etienne Arlaud
 #define SPI_QN_KP   16
 #define SPI_QN_KD   16
 
-static uint8_t my_mac[6] = {0xF8, 0x1A, 0x67, 0xb7, 0xEB, 0x0B};
+/* sensor packet -> status : bits */
+//! \brief System is enabled
+#define SPI_SENSOR_STATUS_SE (1<<15)
+//! \brief Motor 1 is enabled
+#define SPI_SENSOR_STATUS_M1E (1<<14)
+//! \brief Motor 1 is ready
+#define SPI_SENSOR_STATUS_M1R (1<<13)
+//! \brief Motor 2 is enabled
+#define SPI_SENSOR_STATUS_M2E (1<<12)
+//! \brief Motor 2 is ready
+#define SPI_SENSOR_STATUS_M2R (1<<11)
+//! \brief Error code
+#define SPI_SENSOR_STATUS_ERROR (0x0F)
+
+
+static uint8_t my_mac[6] = {0xa0, 0x1d, 0x48, 0x12, 0xa0, 0xc5};//{0xF8, 0x1A, 0x67, 0xb7, 0xEB, 0x0B};
 static uint8_t dest_mac[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 static uint8_t ESP_mac[6] = {0xb4, 0xe6, 0x2d, 0xb5, 0x9f, 0x85}; //{0xcc,0x50,0xe3,0xB6,0xb4,0x58};
 
 LINK_manager *handler;
 uint8_t payload[127];
-
 
 
 struct wifi_eth_packet_command {
@@ -70,12 +85,18 @@ uint16_t adc[2];
 
 struct si_sensor_data {
 	uint16_t status;
-	uint16_t timestamp;
+	uint16_t timestamp;// to be removed
+	bool is_system_enabled; 
+	bool is_motor_enabled[2];
+	bool is_motor_ready[2];
+	uint8_t error_code;  
 	float position[2];
 	float velocity[2];
 	float current[2];
 	float coil_resistance[2];
 	float adc[2];
+	
+	
 }__attribute__ ((packed));
 
 
@@ -90,19 +111,23 @@ void print_hex_table(uint8_t * data,int len)
 
 void convert_raw_to_si_sensor_data(raw_sensor_data raw,si_sensor_data &si)
 {
+    si.is_system_enabled   = raw.status & SPI_SENSOR_STATUS_SE;
+    si.is_motor_enabled[0] = raw.status & SPI_SENSOR_STATUS_M1E;
+    si.is_motor_enabled[1] = raw.status & SPI_SENSOR_STATUS_M2E;
+    si.is_motor_ready[0]   = raw.status & SPI_SENSOR_STATUS_M1R;
+    si.is_motor_ready[1]   = raw.status & SPI_SENSOR_STATUS_M2R;
+	si.error_code          = raw.status & SPI_SENSOR_STATUS_ERROR ;
 	si.position[0]=D32QN_TO_FLOAT(raw.position[0],SPI_QN_POS);
 	si.position[1]=D32QN_TO_FLOAT(raw.position[1],SPI_QN_POS);
 	si.velocity[0]=D32QN_TO_FLOAT(raw.velocity[0],SPI_QN_VEL);
 	si.velocity[1]=D32QN_TO_FLOAT(raw.velocity[1],SPI_QN_VEL);
 	si.current[0]=D32QN_TO_FLOAT(raw.current[0],SPI_QN_IQ);
 	si.current[1]=D32QN_TO_FLOAT(raw.current[1],SPI_QN_IQ);
-	
 }
 
 uint16_t nb_recv = 0;
 
 void callback(uint8_t src_mac[6], uint8_t *data, int len) {
-
 	if (len!=190) 
 	{
 		
@@ -119,7 +144,12 @@ void callback(uint8_t src_mac[6], uint8_t *data, int len) {
 	handler->send();
 	*/
 	//print_hex_table(data,len);
-	if (nb_recv%100 == 1)
+	for(int i=0; i<N_SLAVES;i++)
+	{
+		convert_raw_to_si_sensor_data(uDrivers_raw_sensor_data[i],uDrivers_si_sensor_data[i]);
+	}
+	
+	if (nb_recv%100 == 0)
 	{
 		printf("\e[1;1H\e[2J");
 		for(int i=0; i<N_SLAVES;i++)
@@ -133,20 +163,26 @@ void callback(uint8_t src_mac[6], uint8_t *data, int len) {
 			printf("velB:%8x ",uDrivers_raw_sensor_data[i].velocity[1]);
 			printf("curA:%8x ",uDrivers_raw_sensor_data[i].current[0]);
 			printf("curB:%8x ",uDrivers_raw_sensor_data[i].current[1]);	    
-			convert_raw_to_si_sensor_data(uDrivers_raw_sensor_data[i],uDrivers_si_sensor_data[i]);
+			
 		}
 		printf("\n");
 		for(int i=0; i<N_SLAVES;i++)
 		{
 			printf("\n%d ",i);
-			printf("status:%8x ",uDrivers_si_sensor_data[i].status);
+			printf("err:%x ",uDrivers_si_sensor_data[i].error_code);
+			printf("SE:%d ",uDrivers_si_sensor_data[i].is_system_enabled);
+			printf("M1E:%d ",uDrivers_si_sensor_data[i].is_motor_enabled[0]);
+			printf("M2E:%d ",uDrivers_si_sensor_data[i].is_motor_enabled[1]);
+			printf("M1R:%d ",uDrivers_si_sensor_data[i].is_motor_ready[0]);
+			printf("M2R:%d ",uDrivers_si_sensor_data[i].is_motor_ready[1]);
+			
 			printf("timestamp:%8x ",uDrivers_si_sensor_data[i].timestamp);
-			printf("posA:%8f ",uDrivers_si_sensor_data[i].position[0]);
-			printf("posB:%8f ",uDrivers_si_sensor_data[i].position[1]);
-			printf("velA:%8f ",uDrivers_si_sensor_data[i].velocity[0]);
-			printf("velB:%8f ",uDrivers_si_sensor_data[i].velocity[1]);
-			printf("curA:%8f ",uDrivers_si_sensor_data[i].current[0]);
-			printf("curB:%8f ",uDrivers_si_sensor_data[i].current[1]);
+			printf("pos1:%8f ",uDrivers_si_sensor_data[i].position[0]);
+			printf("pos2:%8f ",uDrivers_si_sensor_data[i].position[1]);
+			printf("vel1:%8f ",uDrivers_si_sensor_data[i].velocity[0]);
+			printf("vel2:%8f ",uDrivers_si_sensor_data[i].velocity[1]);
+			printf("cur1:%8f ",uDrivers_si_sensor_data[i].current[0]);
+			printf("cur2:%8f ",uDrivers_si_sensor_data[i].current[1]);
 			
 		
 		}
@@ -160,16 +196,18 @@ int main(int argc, char **argv) {
 
 	nice(-20);
 
-	handler = new ETHERNET_manager(argv[1], my_mac, dest_mac);
-	//handler = new ESPNOW_manager(argv[1], DATARATE_24Mbps, CHANNEL_freq_9, my_mac, dest_mac, false);
 
-	//((ESPNOW_manager *) handler)->set_filter(my_mac, dest_mac);
+	//~ /*Ethernet*/
+	//~ handler = new ETHERNET_manager(argv[1], my_mac, dest_mac);
+	//~ handler->set_recv_callback(&callback);
+	//~ handler->start();
 
+	//~ /*WiFi*/
+	handler = new ESPNOW_manager(argv[1], DATARATE_24Mbps, CHANNEL_freq_9, my_mac, dest_mac, false);
+	((ESPNOW_manager *) handler)->set_filter(my_mac, dest_mac);
 	handler->set_recv_callback(&callback);
-
 	handler->start();
-
-	//((ESPNOW_manager *) handler)->bind_filter();
+	((ESPNOW_manager *) handler)->bind_filter();
 
 	printf("Payload size : %ld\n", sizeof(wifi_eth_packet_command));
 	memset(&my_command, 0, sizeof(wifi_eth_packet_command));
@@ -179,29 +217,83 @@ int main(int argc, char **argv) {
 	int n_count = 0;
 	std::chrono::time_point<std::chrono::system_clock> last;
 
+
+	int state = 0;
+	int slave_idx = 1;
+	float pos_ref0 = 0;
+	float pos_err0=0;
+	float vel_ref0=0;
+	float vel_err0=0;
+	float Kp = 3.0;
+	float Kd = 1.0;
+	float iq_sat = 2.0;
+	float iq0=0;
+	float PI=3.14;//todo find PI
+	float freq = 2;
+	float t=0;
 	while(1) {
 		if(((std::chrono::duration<double>) (std::chrono::system_clock::now() - last)).count() > 0.001) {
-			if(n_count < 100) {
-				SPI_REG_u16(my_command.command[1], SPI_COMMAND_MODE) = SPI_COMMAND_MODE_ES | SPI_COMMAND_MODE_EM1 | SPI_COMMAND_MODE_EM2 | SPI_COMMAND_MODE_CALIBRATE_M1 | SPI_COMMAND_MODE_CALIBRATE_M2;
 
-				SPI_REG_u16(my_command.command[2], SPI_COMMAND_MODE) = SPI_COMMAND_MODE_ES | SPI_COMMAND_MODE_EM1 | SPI_COMMAND_MODE_EM2 | SPI_COMMAND_MODE_CALIBRATE_M1 | SPI_COMMAND_MODE_CALIBRATE_M2;
-			} else {
-				SPI_REG_u16(my_command.command[1], SPI_COMMAND_MODE) = SPI_COMMAND_MODE_ES | SPI_COMMAND_MODE_EM1 | SPI_COMMAND_MODE_EM2;
-				SPI_REG_16(my_command.command[1], SPI_COMMAND_IQ_1) = FLOAT_TO_D16QN(0.1, SPI_QN_IQ);
-				SPI_REG_16(my_command.command[1], SPI_COMMAND_IQ_2) = FLOAT_TO_D16QN(-0.1, SPI_QN_IQ);
-				
-				SPI_REG_u16(my_command.command[2], SPI_COMMAND_MODE) = SPI_COMMAND_MODE_ES | SPI_COMMAND_MODE_EM1 | SPI_COMMAND_MODE_EM2;
-				SPI_REG_16(my_command.command[2], SPI_COMMAND_IQ_1) = FLOAT_TO_D16QN(0.1, SPI_QN_IQ);
-				SPI_REG_16(my_command.command[2], SPI_COMMAND_IQ_2) = FLOAT_TO_D16QN(-0.1, SPI_QN_IQ);
-			}
-			
 			last = std::chrono::system_clock::now();
-
 			my_command.sensor_index++;
-
 			handler->send((uint8_t *) &my_command, sizeof(wifi_eth_packet_command)),
-
 			n_count++;
+			
+			t +=0.001;
+			switch (state)
+			{
+				case 0:
+					//Initialisation, send the init commands
+					SPI_REG_u16(my_command.command[slave_idx], SPI_COMMAND_MODE) = SPI_COMMAND_MODE_ES | SPI_COMMAND_MODE_EM1 | SPI_COMMAND_MODE_EM2 | SPI_COMMAND_MODE_CALIBRATE_M1 | SPI_COMMAND_MODE_CALIBRATE_M2;
+					//check the end of calibration (are the two motor ready?)
+					if (uDrivers_si_sensor_data[slave_idx].is_motor_enabled[0] && uDrivers_si_sensor_data[slave_idx].is_motor_enabled[1])
+					{
+						state = 1;
+					}
+					break;
+				case 1:
+				    if (!uDrivers_si_sensor_data[slave_idx].is_system_enabled)
+				    {
+						 printf("err");
+						 break;
+					}
+					//Command the motor!
+					//open loop, constant current
+					//~ SPI_REG_u16(my_command.command[slave_idx], SPI_COMMAND_MODE) = SPI_COMMAND_MODE_ES | SPI_COMMAND_MODE_EM1 | SPI_COMMAND_MODE_EM2;
+					//~ SPI_REG_16(my_command.command[slave_idx], SPI_COMMAND_IQ_1) = FLOAT_TO_D16QN(0.1, SPI_QN_IQ);
+					//~ SPI_REG_16(my_command.command[slave_idx], SPI_COMMAND_IQ_2) = FLOAT_TO_D16QN(-0.1, SPI_QN_IQ);
+					
+					//closed loop, position
+					pos_ref0 = sin(2*PI*freq*t);
+					pos_err0 = pos_ref0 - uDrivers_si_sensor_data[slave_idx].position[0];
+					vel_err0 = vel_ref0 - uDrivers_si_sensor_data[slave_idx].velocity[0];
+					iq0 = Kp * pos_err0 + Kd * vel_err0;
+					if (iq0 >  iq_sat) iq0= iq_sat;
+					if (iq0 < -iq_sat) iq0=-iq_sat;
+					
+
+					
+					SPI_REG_u16(my_command.command[slave_idx], SPI_COMMAND_MODE) = SPI_COMMAND_MODE_ES | SPI_COMMAND_MODE_EM1 | SPI_COMMAND_MODE_EM2;
+					SPI_REG_16(my_command.command[slave_idx], SPI_COMMAND_IQ_1) = FLOAT_TO_D16QN(iq0, SPI_QN_IQ);
+					SPI_REG_16(my_command.command[slave_idx], SPI_COMMAND_IQ_2) = FLOAT_TO_D16QN(-0.1, SPI_QN_IQ);
+					
+					break;
+			}
+			//~ if(n_count < 100) {
+				//~ SPI_REG_u16(my_command.command[1], SPI_COMMAND_MODE) = SPI_COMMAND_MODE_ES | SPI_COMMAND_MODE_EM1 | SPI_COMMAND_MODE_EM2 | SPI_COMMAND_MODE_CALIBRATE_M1 | SPI_COMMAND_MODE_CALIBRATE_M2;
+
+				//~ SPI_REG_u16(my_command.command[2], SPI_COMMAND_MODE) = SPI_COMMAND_MODE_ES | SPI_COMMAND_MODE_EM1 | SPI_COMMAND_MODE_EM2 | SPI_COMMAND_MODE_CALIBRATE_M1 | SPI_COMMAND_MODE_CALIBRATE_M2;
+			//~ } else {
+				//~ SPI_REG_u16(my_command.command[1], SPI_COMMAND_MODE) = SPI_COMMAND_MODE_ES | SPI_COMMAND_MODE_EM1 | SPI_COMMAND_MODE_EM2;
+				//~ SPI_REG_16(my_command.command[1], SPI_COMMAND_IQ_1) = FLOAT_TO_D16QN(0.1, SPI_QN_IQ);
+				//~ SPI_REG_16(my_command.command[1], SPI_COMMAND_IQ_2) = FLOAT_TO_D16QN(-0.1, SPI_QN_IQ);
+				
+				//~ SPI_REG_u16(my_command.command[2], SPI_COMMAND_MODE) = SPI_COMMAND_MODE_ES | SPI_COMMAND_MODE_EM1 | SPI_COMMAND_MODE_EM2;
+				//~ SPI_REG_16(my_command.command[2], SPI_COMMAND_IQ_1) = FLOAT_TO_D16QN(0.1, SPI_QN_IQ);
+				//~ SPI_REG_16(my_command.command[2], SPI_COMMAND_IQ_2) = FLOAT_TO_D16QN(-0.1, SPI_QN_IQ);
+			//~ }
+			
+
 		} else {
 			std::this_thread::yield();
 		}
