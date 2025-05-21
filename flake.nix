06b9e86069d255465824d4db7b6708914ce9b7c2
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-05-21 10:37:09 +0200

    nix: set fixed version, set propagatedBuildInputs

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       packages.x86_64-linux.sdk = pkgs.stdenv.mkDerivation {
 
         pname = "odri-masterboard-sdk";
-        version = "nightly";
+        version = "1.0.7";
 
         src = builtins.path {
           name = "sdk";
@@ -26,6 +26,8 @@
           python312
           python312Packages.boost
         ];
+
+        propagatedBuildInputs = with pkgs; [ python312Packages.boost ];
       };
     };
 }
